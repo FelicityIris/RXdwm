6346af574dcb76d18520b77dbccc6bf8eab4e11e
R. T. Redreovich <77635620+Red1922@users.noreply.github.com>
2022-10-02 02:32:21 +0530

    Update autostart.sh

--- Desktop Configuation Files/dwm/autostart.sh
+++ Desktop Configuation Files/dwm/autostart.sh
@@ -12,9 +12,6 @@ slstatus &
 
 #picom -fb --experimental-backends &
 
-# System Tray
-#trayer --align center --SetDockType true --SetPartialStrut true --expand true --width 7 --transparent true --alpha 0 --tint 0x100a40 --height 17 &
-
 # Network-Applet Daemon
 
 nm-applet --indicator &
@@ -38,8 +35,6 @@ st &
 
 sh ~/.config/bspwm/scripts/low_bat_notifier.sh &
 
-# Problem?
-#xsetroot -name "    Red @ Artix Linux [RUNIT] [DWMv6.2] ; This user supports the eternal science of Marxism-Leninism-Maoism, Principally Maoism. Long Live the Masses! Long Live the Revolution! Long Live the Revolutionary Communist Leaders! Combat the vicious Imperialist and Fascist Filth." &
-
 # Wallpaper and LockScreenUpdate
-xwallpaper --zoom ~/Pictures/Wallpapers/SolMount.jpg
+#xwallpaper --zoom ~/Pictures/Wallpapers/SolMount.jpg
+xwallpaper --zoom ~/Pictures/Wallpapers/SolShip1.png
